d390efead433247da74a5635eeedd12ec1111a6a
Yumacide <yumanomics@gmail.com>
2022-08-23 18:27:13 +0200

    Remove unused variable

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ mod cli;
 
 fn main() -> anyhow::Result<()> {
 	let options = Options::parse();
-	let result = match options.command {
+	match options.command {
 		Subcommands::Compile(command) => command.run(),
 		Subcommands::Build(command) => command.run(),
-	};
-	result?;
+	}?;
 	Ok(())
 }
